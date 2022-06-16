ebe8ddab16c0929f62ac9218c0f5824d32491160
raventeee <69177198+raventeee@users.noreply.github.com>
2022-06-17 06:59:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-require('dotenv').config()
+// require('dotenv').config()
 const axios = require('axios')
 const Discord = require('discord.js')
 const client = new Discord.Client()
 function getPrices() {
 	axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=cryptoblades`).then(res => {
-
 		if(res.data && res.data[0].current_price && res.data[0].price_change_percentage_24h) {
 			let currentPrice = res.data[0].current_price || 0 
 			let priceChange = res.data[0].price_change_percentage_24h || 0 
