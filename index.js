0c81be83f4c0f819c36e8d732a0e2f8c24f79302
raventeee <69177198+raventeee@users.noreply.github.com>
2022-08-03 10:54:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ function getPrices() {
 				}
 			})
 
-			client.guilds.find(guild => guild.id === process.env.SERVER_ID1).me.setNickname(`$${(currentPrice*0.93).toLocaleString(undefined, {minimumFractionDigits:3}).replace(/,/g,",")}/SKILL`)
-			client.guilds.find(guild => guild.id === process.env.SERVER_ID2).me.setNickname(`$${(currentPrice*0.93).toLocaleString(undefined, {minimumFractionDigits:3}).replace(/,/g,",")}/SKILL`)
+			client.guilds.find(guild => guild.id === process.env.SERVER_ID1).me.setNickname(`$${(currentPrice*0.95).toLocaleString(undefined, {minimumFractionDigits:3}).replace(/,/g,",")}/SKILL`)
+			client.guilds.find(guild => guild.id === process.env.SERVER_ID2).me.setNickname(`$${(currentPrice*0.95).toLocaleString(undefined, {minimumFractionDigits:3}).replace(/,/g,",")}/SKILL`)
 			console.log('Updated price to', currentPrice)
 		}
 		else
